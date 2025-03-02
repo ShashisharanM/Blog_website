a3165359c94a43b9a8fd2c9ec1f60db849f823b6
Maddana Shashisharan <115500134+ShashisharanM@users.noreply.github.com>
2025-03-02 14:45:20 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+export const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 export const IMAGE_URL = import.meta.env.VITE_IMAGE_URL || "http://localhost:5000/images/";
